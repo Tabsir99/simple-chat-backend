7a3c8a61374c4083b78065ac986ce8c9f5d74885
Tabsir <tabsircg@gmail.com>
2024-11-07 22:42:05 +0600

    add: audio and video call feature

--- src/modules/chats/chat.websocket.ts
+++ src/modules/chats/chat.websocket.ts
@@ -43,8 +43,80 @@ export default class ChatWebSocketHandler implements IConnectionEventHandler {
           event: "user:typing",
           data: ev,
         });
+        console.log("typing...");
       }
     });
 
+    socket.on(
+      "call-offer",
+      ({
+        isVideo,
+        offer,
+        to,
+        caller,
+        callId,
+      }: {
+        offer: RTCSessionDescriptionInit;
+        isVideo: boolean;
+        to: string;
+        callId: string;
+        caller: { username: string; profilePicture: string };
+      }) => {
+        if (!socket.rooms.has(to)) return;
+
+        socket.to(to).emit("callEvent", {
+          event: "call:incoming",
+          data: {
+            from: offer,
+            isVideo,
+            to,
+            caller: {
+              userId: socket.userId,
+              username: caller.username,
+              profilePicture: caller.profilePicture,
+            },
+            callId,
+          },
+        });
+      }
+    );
+
+    socket.on(
+      "decline-call",
+      ({ callId, to }: { callId: string; to: string }) => {
+        if (!socket.rooms.has(to)) return;
+
+        socket.to(to).emit("callEvent", {
+          event: "call:ended",
+          data: {
+            callId
+          }
+        });
+      }
+    );
+
+    socket.on("call-answer",({answer,to}: {answer: RTCSessionDescriptionInit, to: string}) => {
+      console.log(to)
+
+      if(!socket.rooms.has(to)) return
+
+      socket.to(to).emit("callEvent",{
+        event: "call:answered",
+        data: {
+          answer
+        }
+      })
+
+    })
+
+
+    socket.on("ice-candidate",({candidate,chatRoomId}: {chatRoomId: string, candidate: RTCIceCandidate}) => {
+      socket.to(chatRoomId).emit("callEvent",{
+        event: "ice-candidate",
+        data: {
+          candidate
+        }
+      })
+    })
   }
 }
